fb14a7bb1c7aecc6618ea2a1235046d2ab78cd86
JohntPark <JohnTaeHeePark@gmail.com>
2018-12-10 18:46:14 -0800

    First commit

---
+++ main.js
@@ -0,0 +1,36 @@
+$(function() {
+  $("#subscribeBtn").click(checkDetail);
+
+  function checkDetail() {
+    var subscribeDetail = document.getElementById("subscribeGroup");
+    var validInputName = document.getElementById("subscribeName");
+    var validInputEmail = document.getElementById("subscribeEmail");
+    let isValid =
+      validInputName.checkValidity() && validInputEmail.checkValidity();
+
+    if (isValid) {
+      $("#subscribeModal").modal("show");
+      console.log("success");
+    } else {
+      $("#subscribeModal").modal("hide");
+      console.log("failed");
+    }
+  }
+});
+$(function() {
+    $("#feedbackSubmit").click(checkFeedback);
+
+    function checkFeedback() {
+        var feedbackText = document.getElementById("feedbackForm");
+        let isValidText =
+          feedbackText.checkValidity();
+          if(isValidText) {
+              $("#feedback").modal('hide');
+              $("#feedbackModal").modal('show');
+          } else {
+              $("#feedbackModal").modal('hide');
+          }
+    
+    }
+})
+
